7858c246a9dc70b3bdc8d2feddcfb5ebf1d50ff1
Your Nameecho <you@example.com>
2024-10-07 23:54:10 +0530

    frontend initialized with vite+react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-red-500 text-5xl text-center'>welcome</p>
+    </div>
+  )
+}
+
+export default App
